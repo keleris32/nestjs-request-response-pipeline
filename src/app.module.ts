f4acea633c7dfaba850894bbe98c243952ca35ab
keleris32 <charles.ejiegbu@gmail.com>
2022-07-24 02:56:04 +0100

    feat: added guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
+import { APP_GUARD } from '@nestjs/core';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthGuard } from './guards/auth.guard';
 import { AuthenticationMiddleware } from './middleware/authentication.middleware';
 import { RequestService } from './request.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, RequestService],
+  providers: [
+    AppService,
+    RequestService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  private readonly logger = new Logger(AuthGuard.name);
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    this.logger.log(AuthGuard.name);
+
+    const request = context.switchToHttp().getRequest();
+
+    return true;
+  }
+}
