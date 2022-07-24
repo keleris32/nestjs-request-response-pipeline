9d7920a6154d30728de833fe62df3cafd1af0df3
keleris32 <charles.ejiegbu@gmail.com>
2022-07-24 02:50:00 +0100

    feat: added request service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthenticationMiddleware } from './middleware/authentication.middleware';
+import { RequestService } from './request.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RequestService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthenticationMiddleware).forRoutes('*');
+  }
+}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RequestService } from './request.service';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(private readonly requestService: RequestService) {}
+
   getHello(): string {
+    const userId = this.requestService.getUserId();
+
+    this.logger.log('AppService getUserId >>', userId);
     return 'Hello World!';
   }
 }
